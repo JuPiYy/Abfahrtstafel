e60037731f16d5ca15f1e1ae1dfc2f86844b8b5c
Josef <josef@josefunger.de>
2026-07-12 22:32:01 +0200

    Bootstrap hinzugefügt; Routes zum Debugging entfernt;

--- Abfahrtstafel/routes.py
+++ Abfahrtstafel/routes.py
@@ -6,11 +6,3 @@ from flask import render_template
 
 from Abfahrtstafel import app
 from Abfahrtstafel import data
-
-@app.route('/departures')
-def departures():
-    return data.departures()
-
-@app.route('/news')
-def news():
-    return data.news()
